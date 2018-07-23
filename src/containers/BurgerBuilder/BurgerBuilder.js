9b59f4cd7283b69ffdc22e81e1a8d507dc211523
Jet Balagtas <jetbalagtas@gmail.com>
2018-07-22 22:43:41 -0400

    implement backdrop on modal, with click to close

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -76,6 +76,10 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -85,7 +89,7 @@ class BurgerBuilder extends Component {
     } // ex. {salad: true, meat: false, ...}
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} closeModal={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
