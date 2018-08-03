b46e5f300bdbff27abe8600d82e459a56ac09248
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-03 15:53:49 -0400

    protect against memory leaks when reusing HOCs with interceptors; eject the interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,14 +9,24 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     }
 
+    // Warning: these interceptors will be attached multiple times when reusing this HOC to wrap multiple components.
+    // These will cause memory leaks when one of the wrapped components stops being used or is no longer required.
+    // Protect against leaks or undesired behavior by storing references to the interceptors, then...
+    // Eject them with the componentWillUnmount lifecycle method.
     componentWillMount () {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
       });
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({error: error});
       });
+    }
+
+    // To prevent memory leaks when reusing this HOC in wrapping multiple components, eject the interceptors above with:
+    componentWillUnmount () {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
