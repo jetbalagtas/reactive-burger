05a3aa5fdcf933d233a1e639ebc64a1d400d4aa4
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-13 19:01:50 -0400

    fix dropdown error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -89,6 +89,7 @@ class ContactData extends Component {
           ]
         },
         value: '',
+        validation: {},
         valid: true
       },
     },
@@ -120,6 +121,7 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+    if (!rules) return true;
     if (rules.required) {
       isValid = value.trim() !== '' && isValid;
     }
