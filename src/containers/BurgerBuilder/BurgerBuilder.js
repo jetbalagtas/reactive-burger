8de5b30617fe356b8336ef3794e69d4dea8639e3
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-17 18:16:58 -0400

    setup the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,13 +34,13 @@ class BurgerBuilder extends Component {
 
   componentDidMount () {
     console.log(this.props);
-    axios.get('https://reactive-burger-jet.firebaseio.com/ingredients.json')
-    .then(response => {
-      this.setState({ingredients: response.data});
-    })
-    .catch(error => {
-      this.setState({error: true});
-    });
+    // axios.get('https://reactive-burger-jet.firebaseio.com/ingredients.json')
+    // .then(response => {
+    //   this.setState({ingredients: response.data});
+    // })
+    // .catch(error => {
+    //   this.setState({error: true});
+    // });
   }
 
   updatePurchaseState (ingredients) {
