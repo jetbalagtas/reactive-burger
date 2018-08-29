254dd62b4210f832f832a66f65c4494646fba412
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-29 14:57:11 -0400

    store the token and userId

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -9,10 +9,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId
   };
 };
 
@@ -39,7 +40,7 @@ export const auth = (email, password, isSignup) => {
     axios.post(url + key, authData)
     .then(response => {
       console.log(response);
-      dispatch(authSuccess(response.data));
+      dispatch(authSuccess(response.data.idToken, response.data.localId));
     })
     .catch(err => {
       console.log(err);
