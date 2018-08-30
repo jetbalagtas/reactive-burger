575ba39469e9313fc2e57d03aaf9ec4261b0bb17
Jet Balagtas <jetbalagtas@gmail.com>
2018-08-30 14:41:02 -0400

    redirect to Checkout page after build > signin, to Home after build > do not signin, to Signin after this previous redirect on Signin click

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -42,6 +42,7 @@ class BurgerBuilder extends Component {
     if (this.props.isAuthenticated) {
       this.setState({purchasing: true});
     } else {
+      this.props.onSetAuthRedirectPath('/checkout');
       this.props.history.push('/sign-in');
     }
   }
@@ -110,7 +111,8 @@ const mapDispatchToProps = dispatch => {
     onAddIngredient: (ingName) => dispatch(actions.addIngredient(ingName)),
     onRemoveIngredient: (ingName) => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
   }
 }
 
