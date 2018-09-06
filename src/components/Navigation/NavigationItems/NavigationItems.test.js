2702381c47f02b144a71487ce05744e4c583e618
Jet Balagtas <jetbalagtas@gmail.com>
2018-09-05 22:01:59 -0400

    minor edit of it statement

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,7 +23,7 @@ describe('<NavigationItems />', () => {
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
   
-  it('should render three <NavigationItem /> elements if authenticated', () => {
+  it('should render the exact logout nav link if authenticated', () => {
     wrapper.setProps({isAuthenticated: true});
     expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
   });
