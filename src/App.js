9eed9e389653e2f8db7551bc062d64ba9e5798aa
Jet Balagtas <jetbalagtas@gmail.com>
2019-02-09 13:06:53 -0500

    routing with React.lazy()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import('./containers/Checkout/Checkout');
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import('./containers/Orders/Orders');
 });
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import('./containers/Auth/Auth');
 });
 
@@ -27,7 +26,7 @@ const app = props => {
 
   let routes = (
     <Switch>
-      <Route path='/sign-in' component={asyncAuth} />
+      <Route path='/sign-in' render={() => <Auth />} />
       <Route path='/' exact component={BurgerBuilder} />
       <Redirect to='/' /> {/* redirect unknown routes to home */}
     </Switch>
@@ -36,10 +35,10 @@ const app = props => {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path='/checkout' component={asyncCheckout} />
-        <Route path='/orders' component={asyncOrders} />
+        <Route path='/checkout' render={() => <Checkout />} />
+        <Route path='/orders' render={() => <Orders />} />
         <Route path='/logout' component={Logout} />
-        <Route path='/sign-in' component={asyncAuth} />
+        <Route path='/sign-in' render={() => <Auth />} />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' /> {/* redirect unknown routes to home */}
         {/* exact not required with Switch */}
@@ -52,7 +51,9 @@ const app = props => {
   return (
     <div>
       <Layout>
-        {routes}
+        <Suspense fallback={<p>Loading...</p>}>
+          {routes}
+        </Suspense>
       </Layout>
     </div>
   );  
