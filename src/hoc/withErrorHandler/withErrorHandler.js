b6199a60d0877af650bce901ed8e181b0dbbdbd9
Jet Balagtas <jetbalagtas@gmail.com>
2019-02-10 19:06:17 -0500

    convert withErrorHandler HOC to use useState and useEffect hooks

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,49 +1,45 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-  return class extends Component {
-    state = {
-      error: null
-    }
+  return props => {
+    const [error, setError] = useState(null);
 
     // Warning: these interceptors will be attached multiple times when reusing this HOC to wrap multiple components.
     // These will cause memory leaks when one of the wrapped components stops being used or is no longer required.
     // Protect against leaks or undesired behavior by storing references to the interceptors, then...
-    // Eject them with the componentWillUnmount lifecycle method.
-    componentWillMount () {
-      this.reqInterceptor = axios.interceptors.request.use(req => {
-        this.setState({error: null});
-        return req;
-      });
-      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-        this.setState({error: error});
-      });
+    // Clean them up with the useEffect hook below.
+    const reqInterceptor = axios.interceptors.request.use(req => {
+      setError(null);
+      return req;
+    });
+    const resInterceptor = axios.interceptors.response.use(res => res, err => {
+      setError(err);
+    });
+
+    // To prevent memory leaks when reusing this HOC in wrapping multiple components, clean up the interceptors above with:
+    useEffect(() => {
+      return () => {
+        axios.interceptors.request.eject(reqInterceptor);
+        axios.interceptors.response.eject(resInterceptor);
+      }
+    }, [reqInterceptor, resInterceptor]);
+
+    const errorConfirmedHandler = () => {
+      setError(null);
     }
 
-    // To prevent memory leaks when reusing this HOC in wrapping multiple components, eject the interceptors above with:
-    componentWillUnmount () {
-      axios.interceptors.request.eject(this.reqInterceptor);
-      axios.interceptors.response.eject(this.resInterceptor);
-    }
-
-    errorConfirmedHandler = () => {
-      this.setState({error: null});
-    }
-
-    render () {
-      return (
-        <Aux>
-          <Modal show={this.state.error}
-            closeModal={this.errorConfirmedHandler}>
-            {this.state.error ? this.state.error.message : null}
-          </Modal>
-          <WrappedComponent {...this.props} />
-        </Aux>
-      );
-    }
+    return (
+      <Aux>
+        <Modal show={error}
+          closeModal={errorConfirmedHandler}>
+          {error ? error.message : null}
+        </Modal>
+        <WrappedComponent {...props} />
+      </Aux>
+    );
   }
 }
 
